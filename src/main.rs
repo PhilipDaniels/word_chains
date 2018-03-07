9e3f22b866220dc9ebbbe09f9307bd2d59076190
Philip Daniels <Philip.Daniels1971@gmail.com>
2018-03-07 21:29:24 +0000

    More on reachable word calculation.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod dictionary;
 mod reachable_words;
 
 fn main() {
-    dictionary::create_merged_dictionary();
+    //dictionary::create_merged_dictionary();
     reachable_words::calculate_reachable_words();
 }
--- src/reachable_words.rs
+++ src/reachable_words.rs
@@ -1,10 +1,46 @@
 use std::fs;
-use std::collections::HashSet;
+use std::collections::HashMap;
 use std::io::{self, BufRead};
 use std::io::prelude::*;
 
 use ::dictionary;
 
+type WordBase = HashMap<usize, Vec<String>>;
+
 pub fn calculate_reachable_words() {
+    let wordbase = get_words_by_length();
+}
+
+fn get_words_by_length() -> WordBase {
     let f = fs::File::open(dictionary::CORPUS).unwrap();
+    let rdr = io::BufReader::new(f);
+
+    let mut wordbase = WordBase::new();
+
+    {
+        println!("Start reading {}", dictionary::CORPUS);
+
+        for word in rdr.lines() {
+            let word = word.unwrap();
+            let len = word.len();
+            let v = wordbase.entry(len).or_insert(Vec::new());
+            v.push(word);
+        }
+
+        println!("Finished reading {}", dictionary::CORPUS);
+
+        let keys = sorted_keys(&wordbase);
+
+        for key in keys {
+            println!("Number of words of length {:2} = {}", key, wordbase[&key].len());
+        }
+    }
+
+    wordbase
+}
+
+fn sorted_keys(wordbase: &WordBase) -> Vec<usize> {
+    let mut keys : Vec<usize> = wordbase.keys().map(|&k| k).collect();
+    keys.sort();
+    keys
 }
