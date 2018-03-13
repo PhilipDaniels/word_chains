b1312ea767152d56f57ab337f6bfd9d42a305293
Philip Daniels <Philip.Daniels1971@gmail.com>
2018-03-13 22:18:56 +0000

    Refactor letter_differences, introduce AnchoredWords struct.

--- src/letter_differences.rs
+++ src/letter_differences.rs
@@ -6,6 +6,21 @@ use ::dictionary;
 use ::word_base::WordBase;
 use ::word_table::WordTable;
 
+struct AnchoredWords {
+    anchor: String,
+    reachable_words: Vec<String>
+}
+
+impl AnchoredWords {
+    fn new(anchor: String) -> Self {
+        AnchoredWords { anchor: anchor, reachable_words: Vec::new() }
+    }
+
+    fn add_reachable_word(&mut self, word: String) {
+        self.reachable_words.push(word);
+    }
+}
+
 pub fn calculate_words_one_letter_different() {
     let wordbase = get_words_by_length();
 
@@ -14,6 +29,8 @@ pub fn calculate_words_one_letter_different() {
         let word_table = &wordbase[key];
         if word_table.words.len() <= 2 { continue };
         let rwords_for_table = calc_reachable_words_for_table(&word_table);
+        // This is the initial difference file, includes 2-islands.
+        write_difference_file(word_table.word_length(), &rwords_for_table);
     }
 }
 
@@ -41,38 +58,38 @@ fn get_words_by_length() -> WordBase {
     wordbase
 }
 
-fn calc_reachable_words_for_table(wt: &WordTable) -> Vec<Vec<&String>> {
-    let mut all_rwords = Vec::new();
+fn calc_reachable_words_for_table(wt: &WordTable) -> Vec<AnchoredWords> {
+    let mut all_anchored_words = Vec::new();
     for w1 in &wt.words {
-        let mut rwords = Vec::new();
-        // The first word on a line is the anchor word.
-        rwords.push(w1);
-
+        let mut anchored_words = AnchoredWords::new(w1.clone());
         // Followed by all the words that are one letter different.
         for w2 in &wt.words {
             if one_letter_different(w1, w2) {
-                rwords.push(w2);
+                anchored_words.add_reachable_word(w2.clone());
             }
         }
 
-        all_rwords.push(rwords);
+        all_anchored_words.push(anchored_words);
     }
 
-//    let rw_filename = format!("{}/one_letter_different_{:02}.txt", dictionary::DICT_OUT, wt.word_length());
-//    println!("Writing {}", rw_filename);
-//    let rw_file = fs::File::create(rw_filename).unwrap();
-//    let mut writer = io::BufWriter::new(rw_file);
-//    for v in &all_rwords {
-//        // Words which have no other reachable words are not interesting.
-//        if v.len() < 2 { continue };
-//
-//        for w in v.iter() {
-//            write!(writer, "{} ", w).unwrap();
-//        }
-//        write!(writer, "\n").unwrap();
-//    }
+    all_anchored_words
+}
 
-    all_rwords
+fn write_difference_file(word_length: usize, anchored_words: &Vec<AnchoredWords>) {
+    let rw_filename = format!("{}/one_letter_different_{:02}.txt", dictionary::DICT_OUT, word_length);
+    println!("Writing {}", rw_filename);
+    let rw_file = fs::File::create(rw_filename).unwrap();
+    let mut writer = io::BufWriter::new(rw_file);
+    for v in anchored_words {
+        // Words which have no other reachable words are not interesting.
+        if v.reachable_words.len() == 0 { continue };
+
+        write!(writer, "{} ", v.anchor).unwrap();
+        for w in &v.reachable_words {
+            write!(writer, "{} ", w).unwrap();
+        }
+        write!(writer, "\n").unwrap();
+    }
 }
 
 fn one_letter_different(w1: &str, w2: &str) -> bool {
